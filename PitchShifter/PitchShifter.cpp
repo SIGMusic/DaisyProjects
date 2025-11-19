14269c13162b947b22c77f306d85886730982e1a
uiobio <lishenglei2@gmail.com>
2025-11-18 18:55:16 -0600

    mapped parameter controls, implemented pitch shift using libdaisy pitchshifter object

--- PitchShifter/PitchShifter.cpp
+++ PitchShifter/PitchShifter.cpp
@@ -1,15 +1,42 @@
 #include "PitchShifter.h"
 
-float value;
-DaisySeed hw;
 using namespace PitchShifter;
+
+float semiFloat;
+int shiftDir;
+bool bypass;
+
+#define MAX_DELAY static_cast<size_t>(48000 * 0.75f)
+static DelayLine<float, MAX_DELAY> del;
+
+DaisySeed hw;
 
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
-	for (size_t i = 0; i < size; i++)
+	if (bypass)
 	{
-		out[0][i] = in[0][i] * value;
-		out[1][i] = in[1][i] * value;
+		for (size_t i = 0; i < size; i++)
+		{
+			out[0][i] = in[0][i];
+			out[1][i] = in[1][i];
+		}
+	}
+	else
+	{
+		for (size_t i = 0; i < size; i++) // placeholder; need to apply FFT, pitch shift, then IFFT here
+		{
+			float unshifted = in[0][i];
+			float shifted = ps.Process(unshifted);
+
+			float del_out = del.Read();
+			float sig_out = del_out + shifted * 0.5 + unshifted * 0.5;
+
+			float feedback = (del_out * 0.2f) + shifted * 0.5 + unshifted * 0.5;
+			del.Write(feedback);
+
+			out[0][i] = sig_out;
+			out[1][i] = sig_out;
+		}
 	}
 }
 
@@ -18,43 +45,69 @@ int main(void)
 	hw.Init();
 
 	hw.StartLog();
-	hw.SetAudioBlockSize(4); // number of samples handled per callback
+	hw.SetAudioBlockSize(4);
 	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
 
-	AdcChannelConfig adcConfig[NUM_ADC_CHANNELS];
+	del.Init();
+	del.SetDelay(hw.AudioSampleRate() * 0.55f);
 
-	// placeholders
-	adcConfig[semitones].InitSingle(A1);
-	adcConfig[knob2].InitSingle(A2);
-	adcConfig[knob3].InitSingle(A3);
-	adcConfig[knob4].InitSingle(A4);
-	adcConfig[knob5].InitSingle(A5);
-	adcConfig[knob6].InitSingle(A6);
+	ps.Init(hw.AudioSampleRate());
+	ps.SetDelSize(4096);
+	ps.SetTransposition(7);
 
-	hw.adc.Init(adcConfig, NUM_ADC_CHANNELS);
+	pots[SEMITONES].InitSingle(A1);
+	pots[KNOB_2].InitSingle(A2);
+	pots[KNOB_3].InitSingle(A3);
+	pots[KNOB_4].InitSingle(A4);
+	pots[KNOB_5].InitSingle(A5);
+	pots[KNOB_6].InitSingle(A6);
+	hw.adc.Init(pots, NUM_POTS);
+
+	switches[SHIFT_INTERVAL_DIR].Init(D14, D13);
+	switches[SWITCH_2].Init(D7, D11);
+	switches[SWITCH_3].Init(D2, D4);
+
+	buttons[TOGGLE_BYPASS].Init(D25, 1000, Switch::TYPE_TOGGLE, Switch::POLARITY_INVERTED, Switch::PULL_UP);
+	buttons[BUTTON_2].Init(D26, 1000);
+
 	hw.adc.Start();
-
 	hw.StartAudio(AudioCallback);
+	hw.SetLed(!bypass);
+
 	while (1)
 	{
-		value = hw.adc.GetFloat(0);
-		paramToFreqRatio(value, 1);
-		System::Delay(500);
+		buttons[TOGGLE_BYPASS].Debounce();
+		buttons[BUTTON_2].Debounce();
+
+		if (buttons[TOGGLE_BYPASS].RisingEdge())
+		{
+			bypass = !bypass;
+			hw.SetLed(!bypass);
+		}
+
+		semiFloat = hw.adc.GetFloat(0);
+		shiftDir = switches[SHIFT_INTERVAL_DIR].Read();
+		paramToFreqRatio(semiFloat, shiftDir);
 	}
 }
 
-float PitchShifter::paramToFreqRatio(float param, int sign)
+float PitchShifter::paramToFreqRatio(float param, int dir)
 {
 	int semi = static_cast<int>(round(param * 12));
 	// hw.PrintLine("Interval (semitones): %d", semi);
 	float freqRatio = pow(2, (semi / 12));
-	if (sign < 0)
+	if (dir == Switch3::POS_DOWN)
 	{
+		ps.SetTransposition(semi * -1);
+		// hw.PrintLine("Shift dir: DOWN");
 		return 1 / freqRatio;
 	}
-	if (sign > 0)
+	if (dir == Switch3::POS_UP)
 	{
+		ps.SetTransposition(semi);
+		// hw.PrintLine("Shift dir: UP");
 		return freqRatio;
 	}
+	ps.SetTransposition(0);
 	return 1;
 }
--- PitchShifter/PitchShifter.h
+++ PitchShifter/PitchShifter.h
@@ -15,17 +15,43 @@ namespace PitchShifter
 	 * 3 - knob4: placeholder
 	 * 4 - knob5: placeholder
 	 * 5 - knob6: placeholder
-	 * 6 - NUM_ADC_CHANNELS: the number of potentiometers, used for config initialization
+	 * 6 - NUM_ADC_CHANNELS: the number of potentiometers, used for config/initialization
 	 */
-	enum ADCChannel
+	enum Pots
 	{
-		semitones = 0,
-		knob2,
-		knob3,
-		knob4,
-		knob5,
-		knob6,
-		NUM_ADC_CHANNELS,
+		SEMITONES = 0,
+		KNOB_2,
+		KNOB_3,
+		KNOB_4,
+		KNOB_5,
+		KNOB_6,
+		NUM_POTS,
+	};
+
+	/* Switches - channels managed through libDaisy's Switch3 class
+	 * 0 - shiftIntervalDirection: negative if pitch shifting lower, positive if higher, 0 if no shift.
+	 * 1 - switch2: placeholder
+	 * 2 - switch3: placeholder
+	 * 3 - NUM_SWITCHES: the number of switches, used for config/initialization
+	 */
+	enum Switches
+	{
+		SHIFT_INTERVAL_DIR = 0,
+		SWITCH_2,
+		SWITCH_3,
+		NUM_SWITCHES,
+	};
+
+	/* Buttons - channels managed through libDaisy's Switch class
+	 * 0 - toggle: toggles between bypass and active
+	 * 1 - button2: placeholder
+	 * 2 - NUM_BUTTONS: the number of buttons, used for config/initialization
+	 */
+	enum Buttons
+	{
+		TOGGLE_BYPASS = 0,
+		BUTTON_2,
+		NUM_BUTTONS,
 	};
 
 	/* Maps ADC float parameter first to a semitone range (0 to 12)
@@ -34,9 +60,15 @@ namespace PitchShifter
 	 *
 	 * @param float param, the ADC input float from the potentiometer
 	 * @param int sign, the direction in which to scale frequencies, from the 3-way switch.
-	 * 				the middle position of the switch is 0, which works like a bypass. 
+	 * 				the middle position of the switch is 0, which works like a bypass.
 	 *
 	 * @return float, the ratio by which to scale given frequencies
 	 */
-	float paramToFreqRatio(float param, int sign);
+	float paramToFreqRatio(float param, int dir);
+
+	AdcChannelConfig pots[NUM_POTS];
+	vector<Switch3> switches(NUM_SWITCHES, Switch3());
+	vector<Switch> buttons(NUM_BUTTONS, Switch());
+
+	daisysp::PitchShifter ps;
 };
